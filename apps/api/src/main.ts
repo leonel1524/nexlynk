c7cdf3cf783a5cd0bc6e61bc506c52740f1bfcaa
Leonel Matos <matosleonel0@gmail.com>
2026-07-16 14:50:21 -0400

    fix(api): re-enable forbidNonWhitelisted now that UpdateMenuDto has categories

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -26,7 +26,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
-      forbidNonWhitelisted: false,
+      forbidNonWhitelisted: true,
       transform: true,
     }),
   );
